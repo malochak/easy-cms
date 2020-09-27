47df6750fdaeb489a8468bbf7084d849b5b4bd6f
Konrad Małocha <konrad_malocha@hotmail.com>
2020-09-27 17:51:07 +0200

    implement post template

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {ContentComponent} from './content/content.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ContentComponent
+    ContentComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
